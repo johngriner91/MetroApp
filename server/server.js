14bc2f910504fe17e61e092cb5bf4c38c4f917d0
Cody Hazelwood <codyhazelwood@gmail.com>
2015-08-16 14:01:56 -0500

    Add 404 handler.

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ var app = express();
 app.use('/bower_components/', express.static('../bower_components'));
 app.use(express.static('../client'));
 
+app.route('/*').get(function (req, res) {
+    res.status(404).send('404 Page Not Found');
+});
+
 var server = app.listen(8888, function () {
   var host = server.address().address;
   var port = server.address().port;
